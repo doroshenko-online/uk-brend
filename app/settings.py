e9d2283073ae770ef477d83ab224d2cab889d2c3
Dmytro Doroshenko <dmytro.doroshenko@whykingz.com>
2021-11-14 17:38:14 +0200

    change paths

--- app/settings.py
+++ app/settings.py
@@ -1,4 +1,7 @@
+from init import work_directory
+from pathlib import *
+
 APP_HOST = "0.0.0.0"
 APP_PORT = "8443"
-APP_TEMPLATES_PATH = "templates"
-APP_STATIC_PATH = "/static"
+APP_TEMPLATES_PATH = work_directory / 'templates'
+APP_STATIC_PATH = work_directory / "static"
