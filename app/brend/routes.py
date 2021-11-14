4992474de61d79c19c6728b7caf049538ca7788f
Dmytro Doroshenko <dmytro.doroshenko@whykingz.com>
2021-11-14 17:55:37 +0200

    some fixes

--- app/brend/routes.py
+++ app/brend/routes.py
@@ -1,7 +1,8 @@
 from app.brend import views
+from app.settings import APP_STATIC_PATH
 
 
 def setup_routes(app):
-    app.router.add_static('/static/', path='static/', name='static')
+    app.router.add_static('/static/', path=APP_STATIC_PATH, name='static')
     app.router.add_view('/', views.MainPage)
     app.router.add_view('/send_view', views.MainPage)
